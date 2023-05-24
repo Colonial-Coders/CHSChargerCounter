7d5ea3b9dd1d3be9ca4f20a71c87d8407d2c528e
ale <62395331+akidinatophat@users.noreply.github.com>
2023-05-23 22:20:03 -0400

    Add files via upload

--- stuff/main.js
+++ stuff/main.js
@@ -11,6 +11,15 @@ function download(content, fileName, contentType) {
     a.click();
 }
 
+function isJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
 function checkforfull() {
     if (parseInt(document.getElementById("counter1").innerHTML) >= fullbox) {
         var audio = new Audio('/stuff/notif.wav');
@@ -102,8 +111,10 @@ document.getElementById("settings").onclick = function(){
         if (cmd != null) {
             var cmdt = cmd.split("=");
             if (cmdt[0] == "setfullval") {
-                fullbox = parseInt(cmdt[1]);
-                alert("Full Box Value set to " + String(fullbox));
+                if (isNaN(cmdt[1])) {alert("Not a number")} else {
+                    fullbox = parseInt(cmdt[1]);
+                    alert("Full Box Value set to " + String(fullbox));
+                }
             } else if (cmdt[0] == "resetall") {
                 if (confirm("Are you sure you want to reset all counters?") == true) {
                     document.getElementById("counter1").innerHTML = "0";
@@ -139,3 +150,36 @@ document.getElementById("savecounters").onclick = function(){
     }
     download(JSON.stringify(data), 'counters.txt', 'text/plain');
 }
+
+document.getElementById("loadcounters").onclick = function(){
+    if (document.getElementById("countersfile").style.display == "none") {
+        document.getElementById("countersfile").style = "display: block";
+    }
+}
+
+function onChange(event) {
+    var reader = new FileReader();
+    reader.onload = onReaderLoad;
+    reader.readAsText(event.target.files[0]);
+}
+
+function onReaderLoad(event){
+    console.log(event.target.result);
+    if (isJsonString(event.target.result)) {
+    var obj = JSON.parse(event.target.result);
+    if (obj.counter1name != null && obj.counter1value != null) {
+        document.getElementById("countername1").value = obj.counter1name;
+        document.getElementById("counter1").innerHTML = String(obj.counter1value);
+        document.getElementById("countername2").value = obj.counter2name;
+        document.getElementById("counter2").innerHTML = String(obj.counter2value);
+        document.getElementById("countername3").value = obj.counter3name;
+        document.getElementById("counter3").innerHTML = String(obj.counter3value);
+        document.getElementById("countername4").value = obj.counter4name;
+        document.getElementById("counter4").innerHTML = String(obj.counter4value);
+        document.getElementById("countersfile").style = "display: none";
+        alert("Load Success");
+    } else {alert("Invalid JSON File")}
+    } else {alert("Invalid JSON File")}
+}
+
+document.getElementById('countersfile').addEventListener('change', onChange);
